af38e07d1674c04c100f3022b3e1341c196cb64f
Aakash <62670959+aakash2510@users.noreply.github.com>
2021-11-17 01:37:30 +0530

    Create 09. Palindrome Number.cpp

---
+++ Easy/09. Palindrome Number.cpp
@@ -0,0 +1,20 @@
+// Problem Link : https://leetcode.com/problems/palindrome-number/
+
+// Approach 1:
+// Time: O(N)
+// Space: O(1)
+
+class Solution {
+public:
+    bool isPalindrome(int x)
+    {
+        if (x < 0) return false;
+        long long y = x, z = 0;
+        while (x != 0)
+        {
+            z = z * 10 + x % 10;
+            x /= 10;
+        }
+        return (y == z);
+    }
+};
