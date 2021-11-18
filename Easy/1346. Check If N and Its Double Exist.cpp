f93f14270113ed61e5464db1c4119baafa4c93a1
Aakash <62670959+aakash2510@users.noreply.github.com>
2021-11-18 10:30:35 +0530

    Create 1346. Check If N and Its Double Exist.cpp

---
+++ Easy/1346. Check If N and Its Double Exist.cpp
@@ -0,0 +1,49 @@
+// Problem Link : https://leetcode.com/problems/check-if-n-and-its-double-exist/
+
+// Approach 1:
+// Time: O(N^2)
+// Space: O(1)
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) 
+    {
+        bool flag = false;
+        int n= arr.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i!=j)
+                {
+                    if(arr[i]==(2*arr[j]))
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return flag;
+    }
+};
+
+// Approach 2:
+// Time: O(N)
+// Space: O(N)
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr)
+    {
+        int n = arr.size();
+        unordered_set<int> s;
+        for(int i=0;i<n;i++)
+        {
+            if(s.count(2*arr[i]) || (arr[i]%2==0 and s.count(arr[i]/2)))
+            {
+                return true;
+            }
+            s.insert(arr[i]);
+        }
+        return false;
+    }
+};
