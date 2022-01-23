c960e1d1443cb8f9bd062158f2f2b6f0d7ae451f
Aakash <62670959+aakash2510@users.noreply.github.com>
2022-01-23 12:34:52 +0530

    Update 2057. Smallest Index With Equal Value.cpp

--- Easy/2057. Smallest Index With Equal Value.cpp
+++ Easy/2057. Smallest Index With Equal Value.cpp
@@ -1,21 +1,18 @@
 // Problem Link : https://leetcode.com/problems/smallest-index-with-equal-value/
 
-// Approach : 1
 // Time : O(N)
 // Space : O(1)
 
 class Solution {
 public:
-    int smallestEqual(vector<int>& nums)
-    {
-        int n = nums.size();
-        for (int i = 0; i < n; i++)
-        {
-            if (i % 10 == nums[i])
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
+	int smallestEqual(vector<int>& nums) {
+		int n = nums.size(), smallestIndex = -1;
+		for (int i = 0; i < n; ++i) {
+			if (i % 10 == nums[i]) {
+				smallestIndex = i;
+				break;
+			}
+		}
+		return smallestIndex;
+	}
 };
