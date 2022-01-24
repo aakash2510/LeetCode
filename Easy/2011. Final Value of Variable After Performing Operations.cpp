dfba0539643b94b2bec0413ed9002eddad8248e9
Aakash <62670959+aakash2510@users.noreply.github.com>
2022-01-24 23:29:42 +0530

    Update 2011. Final Value of Variable After Performing Operations.cpp

--- Easy/2011. Final Value of Variable After Performing Operations.cpp
+++ Easy/2011. Final Value of Variable After Performing Operations.cpp
@@ -1,43 +1,33 @@
 // Problem Link : https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
 
 // Approach : 1
-// Time: O(N)
-// Space: O(1)
+// Time : O(N)
+// Space : O(1)
 
 class Solution {
 public:
-    int finalValueAfterOperations(vector<string>& operations)
-    {
-        int x = 0;
-        int n = operations.size();
-        for (int i = 0; i < n; i++)
-        {
-            if (operations[i] == "--X" || operations[i] == "X--")
-            {
-                x--;
-            }
-            else {
-                x++;
-            }
-        }
-        return x;
-    }
+	int finalValueAfterOperations(vector<string>& operations) {
+		int n = operations.size(), x = 0;
+		for (int i = 0; i < n; ++i) {
+			if (operations[i] == "++X" or operations[i] == "X++") x++;
+			else x--;
+		}
+		return x;
+	}
 };
 
 // Approach : 2
-// Time: O(N)
-// Space: O(1)
+// Time : O(N)
+// Space : O(1)
 
 class Solution {
 public:
-    int finalValueAfterOperations(vector<string>& operations)
-    {
-        int x = 0;
-        for (auto &s : operations) 
-        {
-            if (s[0] == '+' || s[1] == '+') ++x;
-            else --x;
-        }
-        return x;
-    }
+	int finalValueAfterOperations(vector<string>& operations) {
+		int x = 0;
+		for (auto &s : operations) {
+			if (s[0] == '+' or s[1] == '+') ++x;
+			else --x;
+		}
+		return x;
+	}
 };
