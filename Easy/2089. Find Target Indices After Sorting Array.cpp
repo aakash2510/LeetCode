bd702fb590fd4a7105114255e9ef1bf84708ed4c
Aakash <62670959+aakash2510@users.noreply.github.com>
2022-02-03 13:04:24 +0530

    Update 2089. Find Target Indices After Sorting Array.cpp

--- Easy/2089. Find Target Indices After Sorting Array.cpp
+++ Easy/2089. Find Target Indices After Sorting Array.cpp
@@ -6,20 +6,15 @@
 
 class Solution {
 public:
-    vector<int> targetIndices(vector<int>& nums, int target)
-    {
+	vector<int> targetIndices(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<int> res;
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < n; i++)
-        {
-            if (nums[i] == target)
-            {
-                res.push_back(i);
-            }
-        }
-        return res;
-    }
+		vector<int> ans;
+		sort(nums.begin(), nums.end());
+		for (int i = 0; i < n; ++i) {
+			if (nums[i] == target) ans.push_back(i);
+		}
+		return ans;
+	}
 };
 
 // Approach : 2
